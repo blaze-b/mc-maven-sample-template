6449f511a4327d6581673e20e7c62ea989854529
BRIAN BLAZE <brianblaze14@gmail.com>
2026-03-11 20:21:48 +0000

    code changes

--- src/main/java/com/mclebtec/KataApp.java
+++ src/main/java/com/mclebtec/KataApp.java
@@ -2,10 +2,10 @@ package com.mclebtec;
 
 public class KataApp {
 
-    public String welcome(String name) {
-        String output = "Hello " + name;
-        System.out.println(output);
-        return output;
-    }
+  public String welcome(String name) {
+    String output = "Hello " + name;
+    System.out.println(output);
+    return output;
+  }
 
 }
